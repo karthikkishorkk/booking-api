20a2fffe85f24470fc3a818f82f7e3c2ac30756f
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-04-23 22:37:14 +0530

    Admin DB

---
+++ routes/adminRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const Admin = require('../models/Admin');
+const bcrypt = require('bcryptjs');
+const { generateToken } = require('../Utils/JWT/jwt');
+
+// Admin login route
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.status(400).json({ message: 'Admin not found' });
+    }
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    const token = generateToken(admin._id, 'admin');
+    res.json({ message: 'Admin login successful', token });
+
+  } catch (error) {
+    console.error('Admin login error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// Admin signup route
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingAdmin = await Admin.findOne({ username });
+    if (existingAdmin) {
+      return res.status(400).json({ message: 'Admin username already taken' });
+    }
+
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newAdmin = new Admin({ username, password: hashedPassword });
+    await newAdmin.save();
+
+    res.status(201).json({ message: 'Admin signup successful' });
+  } catch (error) {
+    console.error('Admin signup error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
